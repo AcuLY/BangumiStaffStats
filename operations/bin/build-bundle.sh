d3812f632654165995a7e3f82ddb9b199a26c816
lucaljyang <lucaljyang@tencent.com>
2026-07-29 19:45:37 +0800

    build(ops): pin archive timestamp fix

--- operations/bin/build-bundle.sh
+++ operations/bin/build-bundle.sh
@@ -30,7 +30,7 @@ repository_root="$(
 )"
 # shellcheck source=../lib/common.sh
 source "$repository_root/operations/lib/common.sh"
-accepted_product_revision='7d2aa05853e55499a35d0afd9f6e4cb2dd3be17a'
+accepted_product_revision='6dc7d43f82c44978bf38f348f5d8f9f3efdde147'
 accepted_ci_policy_sha256='0260babc76f71b1fb0730bb84894ce0f3c41c9df93591910f05ac9352ee98176'
 data_version='dv1-0a1fa3e9acdb06be34e3535b3c68e322e7d3f4cd87ac30cd4b608b2276ba3ca1'
 
